fa10f25dfc18073a6e46124ac4a10712fada5d4b
Wallysson Nunes <wally.nm@gmail.com>
2019-05-22 17:17:55 -0300

    Updated code with new data and config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,18 @@
-module.exports = {
-  target: 'serverless'
-}
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+  target: 'serverless',
+  webpack: function (config) {
+    config.module.rules.push({
+      test: /\.(eot|woff|woff2|ttf|svg|png|jpg|gif)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          name: '[name].[ext]'
+        }
+      }
+    })
+    return config
+  }
+})
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
-export default () => (
-  <div className='hello'>
-    <p>Hello World</p>
-    <style jsx>{`
-      .hello {
-        font: 15px Helvetica, Arial, sans-serif;
-        background: #eee;
-        padding: 100px;
-        text-align: center;
-        transition: 100ms ease-in background;
-      }
-      .hello:hover {
-        background: #ccc;
-      }
-    `}</style>
-  </div>
+
+
+import React, { Component } from 'react';
+import 'semantic-ui-css/semantic.min.css'
+import Layout from './components/Layout'
+import Gallery from './components/Gallery'
+import axios from 'axios'
+
+const Index = ({ cats }) => (
+  <Layout>
+    <Gallery cats={cats}/>
+  </Layout>
 )
+
+Index.getInitialProps = async function() {
+  const result = await axios.get('https://api.thecatapi.com/v1/images/search?limit=25')
+  return {
+    'cats': result.data
+  }
+}
+
+export default Index
